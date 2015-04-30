e01d587116c7d89644e0593ce8ed5a38fe18e13f
jennyheath <jennifer.w.heath@gmail.com>
2015-04-30 16:17:59 -0700

    Outline Game class; Change display; Refactor Board class

--- w2d4/board.rb
+++ w2d4/board.rb
@@ -19,29 +19,29 @@ class CheckerBoard
     @grid[row][col] = piece
   end
 
-  def set_board
+  def each_tile_with_index(&prc)
     @grid.each_with_index do |row, row_idx|
       row.each_with_index do |tile, col_idx|
-        pos = [row_idx, col_idx]
-        if row_idx <= 2
-          self[pos] = Piece.new(self, :black, pos) if (row_idx - col_idx).even?
-        elsif row_idx >= 5
-          self[pos] = Piece.new(self, :red, pos) if (row_idx - col_idx).even?
-        end
+        prc.call(row, row_idx, tile, col_idx)
       end
     end
   end
 
-  # def each_tile_with_index(&prc)
-  #   @grid.each_with_index do |row, row_idx|
-  #     row.each_with_index do |tile, col_idx|
-  #       prc.call(row, row_idx, tile, col_idx)
-  #     end
-  #   end
-  # end
+  def set_board
+    each_tile_with_index do |row, row_idx, tile, col_idx|
+      pos = [row_idx, col_idx]
+      if row_idx <= 2
+        self[pos] = Piece.new(self, :black, pos) if (row_idx - col_idx).even?
+      elsif row_idx >= 5
+        self[pos] = Piece.new(self, :red, pos) if (row_idx - col_idx).even?
+      end
+    end
+  end
 
   def display
+    puts "   1  2  3  4  5  6  7  8 "
     @grid.each_with_index do |row, row_idx|
+      print "#{row_idx + 1} "
       row.each_with_index do |tile, col_idx|
         if tile.nil?
           print "   ".colorize(background: :black) if (row_idx - col_idx).even?
@@ -57,12 +57,10 @@ class CheckerBoard
 
   def dup_board
     test_board = CheckerBoard.new
-    @grid.each_with_index do |row, row_idx|
-      row.each_with_index do |tile, col_idx|
-        if !tile.nil?
-          pos = [row_idx, col_idx]
-          test_board[pos] = Piece.new(test_board, tile.color, tile.pos.dup, tile.king)
-        end
+    each_tile_with_index do |row, row_idx, tile, col_idx|
+      if !tile.nil?
+        pos = [row_idx, col_idx]
+        test_board[pos] = Piece.new(test_board, tile.color, tile.pos.dup, tile.king)
       end
     end
     test_board
@@ -71,7 +69,7 @@ end
 
 game = CheckerBoard.new
 game.set_board
-# game.display
+game.display
 
 game[[2,0]].perform_slide([2,0], [3,1])
 puts
