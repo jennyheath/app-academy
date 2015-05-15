2c5585e31c6f70fe33501a70db5b44850a81114e
App Academy Student <student@appacademy.io>
2015-05-15 13:33:16 -0700

    add users and subs

---
+++ w4d5/RedditClone/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      render json: @user
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+end
