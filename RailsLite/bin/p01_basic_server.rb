8eb6ecf848cead5b0d7b38cd30d9778cd5d96898
jennyheath <jennifer.w.heath@gmail.com>
2015-05-19 13:57:15 -0700

    Make RailsLite skeleton

---
+++ RailsLite/bin/p01_basic_server.rb
@@ -0,0 +1,8 @@
+require 'webrick'
+
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+
+server = WEBrick::HTTPServer.new(Port: 3000)
